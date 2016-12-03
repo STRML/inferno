57abed0fe7578a5b383cbc52a304b036c807cd00
Luke Sheard <me@lukesheard.com>
2016-12-03 03:54:22 +0000

    Check if running Sauce

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -166,6 +166,7 @@ module.exports = function (config) {
 	}
 
 	if (process.env.TRAVIS && !process.env.TRAVIS_PULL_REQUEST) {
+		console.log("Running with Sauce Labs");
 		if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
 			console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.')
 			process.exit(1)
